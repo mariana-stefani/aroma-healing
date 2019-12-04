dfe2dc7309b9cd15b4fefa8a42baaa6d76aab20d
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2019-12-04 19:17:24 +0000

    Cloned index.html

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,44 +1,30 @@
+
 var markerData = [
     //France
-    { lat: 48.864716, lng: 2.349014, zoom: 5, name: "France", id: "#france" },
-    //Brazil
-    { lat: -15.793889, lng: -47.882778, zoom: 5, name: "Brazil" },
-    //Egypt
-    { lat: 30.045916, lng: 31.224291, zoom: 5, name: "Egypt" },
-    //China
-    { lat: 39.916668, lng: 116.383331, zoom: 5, name: "China" },
-    //India
-    { lat: 28.644800, lng: 77.216721, zoom: 5, name: "India" }
-
-];
-
-
-//France
-//Courses
-var markerData = [
+    //Courses
     {
         icon: icon,
         content: '<div id="web-info"> <h6><a target="_blank" href="https://www.ipal-formation.com/">Ipal</a></h6><p>Artomatherapy and Essential Oils Training</p></div>',
-        position: {lat: 43.583627, lng: 3.814796}
+        position: { lat: 43.583627, lng: 3.814796 }
     },
     {
         icon: icon,
         content: '<div id="web-info"> <h6><a target="_blank" href="http://www.ecole-era.ch/">Ecole Romande d\'Aromathérapie ERA</a></h6><p>Aromatherapy Course</p></div>',
-        position: {lat: 46.521448, lng: 6.633112}
+        position: { lat: 46.521448, lng: 6.633112 }
     },
     {
         icon: icon,
         content: '<div id="web-info"> <h6><a target="_blank" href="https://www.formationaromatherapie.com/">French Aromatherapy Federation</a></h6><p>Aromatherapy and Essential Oils Training</p></div>',
-        position: {lat: 43.651153, lng: 1.513574}
+        position: { lat: 43.651153, lng: 1.513574 }
     },
     {
         icon: icon,
         content: '<div id="web-info"> <h6><a target="_blank" href="https://www.ecole-aroma.com/">French School  of Integrative Aromatherapy</a></h6><p>Integrative Aromatherapy Practitioner Training</p></div>',
-        position: {lat: 48.829997, lng: 2.227497}
+        position: { lat: 48.829997, lng: 2.227497 }
     },
     {
         icon: icon,
         content: '<div id="web-info"> <h6><a target="_blank" href="https://www.laromatheque.fr/">L\'Aromathèque Jacobins</a></h6><p>Complete Essential Oils Training</p></div>',
-        position: {lat: 45.761047, lng: 4.832847}
+        position: { lat: 45.761047, lng: 4.832847 }
     }
 ];
