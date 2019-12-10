71cbc710e4846b54b8b45c7b86e1fdd9ec89597e
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2019-12-10 22:37:18 +0000

    Added description to markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,28 +1,29 @@
-
 var markers = [
     {
-        "title": 'Frace',
-        "lat": '48.864716',
-        "lng": '2.349014',
-        "description": 'This is France',
+        "title": 'France',
+        "lat": '43.583627',
+        "lng": '3.814796 ',
+        "description": '<div id="web-info"> <h6><a target="_blank" href="https://www.ipal-formation.com/">Ipal</a></h6><p>Artomatherapy and Essential Oils Training</p></div>',
         "zoom": '5'
     },
     {
-        "title": 'Brazil',
-        "lat": '-15.793889',
-        "lng": '-47.882778',
-        "description": 'This is Brazil',
+        "title": 'France',
+        "lat": '46.521448',
+        "lng": '6.633112',
+        "description": '<div id="web-info"> <h6><a target="_blank" href="http://www.ecole-era.ch/">Ecole Romande d\'Aromathérapie ERA</a></h6><p>Aromatherapy Course</p></div>',
         "zoom": '5'
     },
     {
-        "title": 'Egypt',
-        "lat": '30.045916',
-        "lng": '31.224291',
-        "description": 'This is Egypt',
+        "title": 'France',
+        "lat": '43.651153',
+        "lng": '1.513574',
+        "description": '<div id="web-info"> <h6><a target="_blank" href="https://www.formationaromatherapie.com/">French Aromatherapy Federation</a></h6><p>Aromatherapy and Essential Oils Training</p></div>',
         "zoom": '5'
     }
 
 ];
+
+
 
 function initMap() {
     var mapOptions = {
@@ -36,26 +37,28 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
-    //Code from https://www.aspsnippets.com/Articles/Google-Maps-API-V3-Add-multiple-markers-with-InfoWindow-to-Google-Map.aspx
+    //Tutorial from https://www.aspsnippets.com/Articles/Google-Maps-API-V3-Add-multiple-markers-with-InfoWindow-to-Google-Map.aspx
     //Created infoWindow 
-    var infoWindow = new google.maps.InfoWindow();
+    var infowindow = new google.maps.InfoWindow();
 
     for (var i = 0; i < markers.length; i++) {
-        var data = markers[i];
-        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+        var markersData = markers[i];
+        var coords = new google.maps.LatLng(markersData.lat, markersData.lng);
         var marker = new google.maps.Marker({
-            position: myLatlng,
+            position: coords,
             map: map,
-            title: data.title
+            title: markersData.title
         });
 
         //Added click listener
-        (function (marker, data) {
+        (function (marker, markersData) {
             google.maps.event.addListener(marker, "click", function (e) {
-                infoWindow.setContent(data.description);
-                infoWindow.open(map, marker);
+                infowindow.setContent(markersData.description);
+                infowindow.open(map, marker);
             });
-            jQuery("#selectlocation").append('<option value="' + [data.lat, data.lng, data.zoom].join('|') + '">' + data.title + '</option>');
+
+            //Code from http://bl.ocks.org/amenadiel/353e4d04d4b2923c438e
+            jQuery("#selectlocation").append('<option value="' + [markersData.lat, markersData.lng, markersData.zoom].join('|') + '">' + markersData.title + '</option>');
             jQuery(document).on('change', '#selectlocation', function () {
                 var latlngzoom = jQuery(this).val().split('|');
                 var newzoom = 1 * latlngzoom[2],
@@ -64,48 +67,23 @@ function initMap() {
                 map.setZoom(newzoom);
                 map.setCenter({ lat: newlat, lng: newlng });
             });
-        })(marker, data);
+        })(marker, markersData);
     }
-}
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // markerData.forEach(function (data) {
-// //     var newmarker = new google.maps.Marker({
-// //         map: map,
-// //         position: { lat: data.lat, lng: data.lng }
-// //     });
-
-// //     jQuery("#selectlocation").append('<option value="' + [data.lat, data.lng, data.zoom].join('|') + '">' + data.name + '</option>');
-// // });
-
-// jQuery(document).on('change', '#selectlocation', function () {
-//     var latlngzoom = jQuery(this).val().split('|');
-//     var newzoom = 1 * latlngzoom[2],
-//         newlat = 1 * latlngzoom[0],
-//         newlng = 1 * latlngzoom[1];
-//     map.setZoom(newzoom);
-//     map.setCenter({ lat: newlat, lng: newlng });
-// });
-// // }
+//France
+    //Courses
+    
+//    
+//     {
+//         // icon: icon,
+//         content: '<div id="web-info"> <h6><a target="_blank" href="https://www.ecole-aroma.com/">French School  of Integrative Aromatherapy</a></h6><p>Integrative Aromatherapy Practitioner Training</p></div>',
+//         position: { lat: 48.829997, lng: 2.227497 }
+//     },
+//     {
+//         // icon: icon,
+//         content: '<div id="web-info"> <h6><a target="_blank" href="https://www.laromatheque.fr/">L\'Aromathèque Jacobins</a></h6><p>Complete Essential Oils Training</p></div>',
+//         position: { lat: 45.761047, lng: 4.832847 }
+//     }
+// ];
