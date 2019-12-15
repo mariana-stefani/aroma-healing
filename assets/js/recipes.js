0debf02b68b572debb7c41e299b4256d20b6c7a9
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2019-12-15 21:16:00 +0000

    Adjusting pie chart

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -16,8 +16,8 @@ var svg = d3.select("#recipe-graph")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // create 2 data_set
-var data1 = {a: 9, b: 20, c:30, d:8, e:12}
-var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
+var data1 = {a:56.2, b: 25, c:18.8}
+var data2 = {c: 50, d: 31.2, e:18.8}
 
 // set the color scale
 var color = d3.scaleOrdinal()
