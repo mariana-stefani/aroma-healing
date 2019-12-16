5aa8790ee02d716eb3383169f281888a78ee9564
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2019-12-16 17:00:39 +0000

    Adding relevant comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-//Get the button:
+//Back to top Btn
+// Tutorial from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+//Creating the button
 mybutton = document.getElementById("topBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down to "Aromatherapy Courses..." the button will show
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 950 || document.documentElement.scrollTop > 950) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// Function to go back to top
 function backtoTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
